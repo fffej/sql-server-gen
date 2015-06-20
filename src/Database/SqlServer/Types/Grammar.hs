6905f6dcfd97821c826f2673638c536099200c7d
Jeff Foster <jeff.foster@acm.org>
2015-06-20 17:19:10 +0100

    generate arbitrary tables

--- src/Database/SqlServer/Types/Grammar.hs
+++ src/Database/SqlServer/Types/Grammar.hs
@@ -8,11 +8,14 @@ import Database.SqlServer.Types.Reserved (isReserved)
 import Test.QuickCheck
 import Test.QuickCheck.Gen
 import Control.Monad
+import Data.List (nub,intersperse)
+
 import Data.DeriveTH
 
 
+
 -- https://msdn.microsoft.com/en-us/subscriptions/downloads/ms175874
-newtype RegularIdentifier = RegularIdentifier String 
+newtype RegularIdentifier = RegularIdentifier String deriving Eq
 
 firstChars :: String
 firstChars = ['a'..'z'] ++ ['A'..'Z'] ++ "_@#"
@@ -22,6 +25,9 @@ subsequentChars = firstChars ++ ['0'..'9']
 
 maximumLengthOfRegularIdentifier :: Int
 maximumLengthOfRegularIdentifier = 128
+
+validLength :: String -> Bool
+validLength x = length x < maximumLengthOfRegularIdentifier
 
 -- https://msdn.microsoft.com/en-us/library/ms189822.aspx
 newtype Keyword = Keyword String
@@ -70,12 +76,17 @@ data Type = BigInt
           | Geography
           | Geometry
 
+newtype ColumnDefinitions = ColumnDefinitions [ColumnDefinition]
+
 -- https://msdn.microsoft.com/en-us/library/ms174979.aspx
 data TableDefinition = TableDefinition
              {
                table_name    :: RegularIdentifier
-             , column_definitions :: [ColumnDefinition]
+             , column_definitions :: ColumnDefinitions
              }
+
+uniqueNames :: [ColumnDefinition] -> Bool
+uniqueNames xs = length xs == length (nub $ map column_name xs)
 
 data ColumnDefinition = ColumnDefinition
                         {
@@ -90,8 +101,11 @@ instance Arbitrary FixedRange where
 instance Arbitrary RegularIdentifier where
   arbitrary = do
     x <- elements firstChars
-    y <- listOf (elements subsequentChars) `suchThat` (\q -> length q < maximumLengthOfRegularIdentifier)
+    y <- listOf (elements subsequentChars) `suchThat` validLength
     return (RegularIdentifier $ x : y)
+
+instance Arbitrary ColumnDefinitions where
+  arbitrary = liftM ColumnDefinitions $ listOf1 arbitrary
     
 derive makeArbitrary ''Type
 
@@ -114,8 +128,15 @@ instance Show Range where
 instance Show Type where
   show _ = "type"
 
+instance Show ColumnDefinitions where
+  show (ColumnDefinitions xs) = concat $ intersperse ",\n" $ map show xs
+
 instance Show ColumnDefinition where
   show c = show (column_name c) ++ " " ++ show (data_type c) ++
            nullConstraint
     where
       nullConstraint = maybe "" (\x -> if x then " NULL" else " NOT NULL") $ null_constraint c
+
+instance Show TableDefinition where
+  show t = "CREATE TABLE " ++ show (table_name t) ++
+           "(" ++ show (column_definitions t) ++  ")"
