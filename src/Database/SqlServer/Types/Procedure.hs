75412927891aea8632d3a0e6887c263bf561198c
Jeff Foster <jeff.foster@acm.org>
2015-06-27 21:42:51 +0100

    add valid procedures

---
+++ src/Database/SqlServer/Types/Procedure.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
+
+module Database.SqlServer.Types.Procedure where
+
+import Database.SqlServer.Types.Identifiers hiding (unwrap)
+import Database.SqlServer.Types.DataTypes
+import Database.SqlServer.Types.Properties
+
+import Test.QuickCheck
+import Data.DeriveTH
+import Data.Word (Word16)
+import Text.PrettyPrint
+import Data.Maybe (isJust)
+import Control.Monad
+
+import qualified Data.Set as S
+
+data ExecuteAsClause = Owner
+                     | Self
+
+derive makeArbitrary ''ExecuteAsClause
+
+data ProcedureOption = Encryption
+                     | Recompile
+                     | ExecuteAs ExecuteAsClause
+
+derive makeArbitrary ''ProcedureOption
+
+type ProcedureOptions = S.Set ProcedureOption
+
+newtype ParameterIdentifier = ParameterIdentifier { unwrapP :: RegularIdentifier }
+
+derive makeArbitrary ''ParameterIdentifier
+
+renderParameterIdentifier :: ParameterIdentifier -> Doc
+renderParameterIdentifier (ParameterIdentifier p) = text "@" <> renderRegularIdentifier p
+
+data Parameter = Parameter
+  {
+    parameterName :: ParameterIdentifier
+  , dataType      :: Type
+  , isOutput      :: Maybe Bool
+  }
+
+instance NamedEntity Parameter where
+  name = unwrapP . parameterName
+
+derive makeArbitrary ''Parameter
+
+renderParameter :: Parameter -> Doc
+renderParameter p = renderParameterIdentifier (parameterName p) <+> renderDataType (dataType p)
+
+newtype Parameters = Parameters { unwrap :: [Parameter] } 
+
+instance Arbitrary Parameters where
+  arbitrary = liftM Parameters (listOf arbitrary `suchThat` validIdentifiers)
+
+data ProcedureDefinition = ProcedureDefinition
+  {
+    procedureName :: RegularIdentifier
+  , parameters    :: Parameters
+  }
+
+derive makeArbitrary ''ProcedureDefinition
+
+-- Generating arbitrary SQL is perhaps a bit complicated.
+statementBody :: String
+statementBody = "select 1\nGO\n"
+
+renderProcedureDefinition :: ProcedureDefinition -> Doc
+renderProcedureDefinition p = text "CREATE PROCEDURE" <+> renderRegularIdentifier (procedureName p) $+$
+                              hcat (punctuate comma (map renderParameter (unwrap $ parameters p))) <+> text "AS" $+$
+                              text statementBody
+                              
+                                                    
