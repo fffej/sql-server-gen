8109143d4b606fb099782b9c92470c3d2c07bad0
Jeff Foster <jeff.foster@acm.org>
2015-06-28 17:53:21 +0100

    clean up warnings.

--- src/Database/SqlServer/Types/Properties.hs
+++ src/Database/SqlServer/Types/Properties.hs
@@ -2,15 +2,9 @@ module Database.SqlServer.Types.Properties where
 
 import Database.SqlServer.Types.Identifiers (RegularIdentifier, unwrap)
 import Database.SqlServer.Types.Reserved (isReserved)
-import Data.Char (toUpper)
-
-import Data.List (nub)
 
 class NamedEntity a where
   name :: a -> RegularIdentifier
-
-uniqueNames :: NamedEntity a => [a] -> Bool
-uniqueNames xs = length xs == length (nub $ map (\x -> map toUpper (unwrap $ name x)) xs)
 
 reserved :: NamedEntity a => a -> Bool
 reserved a = isReserved $ unwrap (name a)
@@ -19,7 +13,7 @@ unReserved :: NamedEntity a => a -> Bool
 unReserved = not . reserved
 
 validIdentifiers :: NamedEntity a => [a] -> Bool
-validIdentifiers xs = all unReserved xs && uniqueNames xs
+validIdentifiers xs = all unReserved xs
 
 
   
