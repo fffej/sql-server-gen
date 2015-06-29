81f8535588c3df87f6f57a7a6d77907e7ca3f825
Jeff Foster <jeff.foster@acm.org>
2015-06-29 08:36:40 +0100

    initial version

--- web/Main.hs
+++ web/Main.hs
@@ -1,21 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 import Web.Scotty
-import Data.Monoid (mconcat)
+import Data.Aeson hiding (json)
+import GHC.Generics
+import Data.Hashable
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+import Test.QuickCheck.Random
+import Text.PrettyPrint (render)
+import Control.Monad.IO.Class
+import Data.Text
+
+
+import Database.SqlServer.Types.Database;
+
+data DatabaseAsJson = DatabaseAsJson
+   {
+     dbName :: String
+   , seed   :: String
+   , createStatement :: String
+   } deriving Generic
+
+instance ToJSON DatabaseAsJson
+
+generateDB :: String -> String -> IO String
+generateDB n s = do
+  let seed = hash s    
+      db = unGen (arbitrary :: Gen DatabaseDefinition) (mkQCGen seed) seed
+  return $ (render . renderDatabaseDefinition) db
 
 main :: IO ()
 main = scotty 8888 $ do
-  get "/database/:database/:hash" $ do
-    dbName <- param "database"
-    hash   <- param "hash"
-    html $ mconcat [
-      "<h1>Database: ",
-      dbName,
-      "</h1>",
-      "<h1>Hash: ",
-      hash,
-      "</h1>"
-      ]
+  get "/database/:database/:seed" $ do
+    n <- param "database"
+    s   <- param "seed"
+    db <- liftIO $ generateDB n s
+    text db
                     
