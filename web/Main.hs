e08072be7065c16cf6e1d711a6d33e06b9166d07
Jeff Foster <jeff.foster@acm.org>
2015-07-02 20:44:24 +0100

    simplify the scotty implementation

--- web/Main.hs
+++ web/Main.hs
@@ -6,37 +6,37 @@ module Main where
 import Web.Scotty
 import Data.Aeson hiding (json)
 import GHC.Generics
-import Data.Hashable
 import Test.QuickCheck
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Random
 import Text.PrettyPrint (render)
-import Control.Monad.IO.Class
-import Data.Text
 
 
 import Database.SqlServer.Types.Database;
 
 data DatabaseAsJson = DatabaseAsJson
    {
-     dbName :: String
-   , seed   :: String
+     seed   :: Int
+   , size   :: Int
    , createStatement :: String
    } deriving Generic
 
 instance ToJSON DatabaseAsJson
 
-generateDB :: String -> String -> IO String
-generateDB n s = do
-  let seed = hash s    
-      db = unGen (arbitrary :: Gen DatabaseDefinition) (mkQCGen seed) seed
-  return $ (render . renderDatabaseDefinition) db
+generateDB :: Int -> Int -> DatabaseAsJson
+generateDB seed' size' = DatabaseAsJson
+    {
+      seed = seed'
+    , size = size'
+    , createStatement = (render . renderDatabaseDefinition) db
+    }
+  where
+    db = unGen (arbitrary :: Gen DatabaseDefinition) (mkQCGen seed') size'
 
 main :: IO ()
 main = scotty 8888 $ do
-  get "/database/:database/:seed" $ do
-    n <- param "database"
-    s   <- param "seed"
-    db <- liftIO $ generateDB n s
-    text db
+  get "/database/:seed" $ do
+    s <- param "seed"
+    z <- param "size" `rescue` (const $ return 101)
+    json (generateDB s z)
                     
