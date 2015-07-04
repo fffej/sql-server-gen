a3941ab30ca92bd966628a073d05f127f55e6206
Jeff Foster <jeff.foster@acm.org>
2015-07-04 12:35:25 +0100

    simplify some more.

--- src/Database/SqlServer/Types/Database.hs
+++ src/Database/SqlServer/Types/Database.hs
@@ -9,35 +9,29 @@ import Database.SqlServer.Types.Queue
 import Test.QuickCheck
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Random
-import Control.Monad
 
 import Text.PrettyPrint
-
-newtype TableDefinitions = TableDefinitions [TableDefinition]
-newtype SequenceDefinitions = SequenceDefinitions [SequenceDefinition]
-newtype ProcedureDefinitions = ProcedureDefinitions [ProcedureDefinition]
-newtype QueueDefinitions = QueueDefinitions [QueueDefinition]
 
 data DatabaseDefinition = DatabaseDefinition
                           {
                             databaseName :: RegularIdentifier
-                          , tableDefinitions :: TableDefinitions
-                          , sequenceDefinitions :: SequenceDefinitions
-                          , procedureDefinitions :: ProcedureDefinitions
-                          , queueDefinitions :: QueueDefinitions
+                          , tableDefinitions :: [TableDefinition]
+                          , sequenceDefinitions :: [SequenceDefinition]
+                          , procedureDefinitions :: [ProcedureDefinition]
+                          , queueDefinitions :: [QueueDefinition]
                           }
 
-renderTableDefinitions :: TableDefinitions -> Doc
-renderTableDefinitions (TableDefinitions xs) = vcat (map renderTableDefinition (S.toList xs))
+renderTableDefiniton :: [TableDefinition] -> Doc
+renderTableDefiniton xs = vcat (map renderTableDefinition xs)
 
-renderSequenceDefinitions :: SequenceDefinitions -> Doc
-renderSequenceDefinitions (SequenceDefinitions xs) = vcat (map renderSequenceDefinition (S.toList xs))
+renderSequenceDefinitions :: [SequenceDefinition] -> Doc
+renderSequenceDefinitions xs = vcat (map renderSequenceDefinition xs)
 
-renderProcedureDefinitions :: ProcedureDefinitions -> Doc
-renderProcedureDefinitions (ProcedureDefinitions xs) = vcat (map renderProcedureDefinition (S.toList xs))
+renderProcedureDefinitions :: [ProcedureDefinition] -> Doc
+renderProcedureDefinitions xs = vcat (map renderProcedureDefinition xs)
 
-renderQueueDefinitions :: QueueDefinitions -> Doc
-renderQueueDefinitions (QueueDefinitions xs) = vcat (map renderQueueDefinition (S.toList xs))
+renderQueueDefinitions :: [QueueDefinition] -> Doc
+renderQueueDefinitions xs = vcat (map renderQueueDefinition xs)
 
 renderDatabaseDefinition :: DatabaseDefinition -> Doc
 renderDatabaseDefinition  dd = text "USE master" $+$
@@ -45,33 +39,21 @@ renderDatabaseDefinition  dd = text "USE master" $+$
                                text "CREATE DATABASE" <+> dbName $+$
                                text "GO" $+$
                                text "USE" <+> dbName $+$
-                               renderTableDefinitions (tableDefinitions dd) $+$
+                               renderTableDefiniton (tableDefinitions dd) $+$
                                renderSequenceDefinitions (sequenceDefinitions dd) $+$
                                renderProcedureDefinitions (procedureDefinitions dd) $+$
                                renderQueueDefinitions (queueDefinitions dd)
   where
     dbName = renderRegularIdentifier (databaseName dd)
 
-instance Arbitrary TableDefinitions where
-  arbitrary = liftM (TableDefinitions . S.fromList) (listOf1 arbitrary)
-
--- TODO remove duplication
-makeArbitraryProcs :: Gen (S.Set ProcedureDefinition)
-makeArbitraryProcs = liftM S.fromList arbitrary
-
-makeArbitrarySeqs :: Gen (S.Set SequenceDefinition)
-makeArbitrarySeqs  = liftM S.fromList arbitrary
-
-makeArbitraryQueues :: Gen (S.Set QueueDefinition)
-makeArbitraryQueues = liftM S.fromList arbitrary
-
+-- TODO derive arbitrary
 instance Arbitrary DatabaseDefinition where
   arbitrary = do
     dbName <- arbitrary
     tables <- arbitrary
-    sequences <- liftM SequenceDefinitions makeArbitrarySeqs
-    procs <- liftM ProcedureDefinitions makeArbitraryProcs
-    queues <- liftM QueueDefinitions makeArbitraryQueues 
+    sequences <- arbitrary
+    procs <- arbitrary
+    queues <-arbitrary
     return $ DatabaseDefinition
       {
         databaseName = dbName
