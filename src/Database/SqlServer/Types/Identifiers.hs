a71fb9eb5e79759e658c87039e53c3d394987688
Jeff Foster <jeff.foster@acm.org>
2015-07-04 11:44:03 +0100

    make random identifiers

--- src/Database/SqlServer/Types/Identifiers.hs
+++ src/Database/SqlServer/Types/Identifiers.hs
@@ -1,57 +1,45 @@
 module Database.SqlServer.Types.Identifiers where
 
-import Database.SqlServer.Types.Reserved (isReserved)
-
 import Test.QuickCheck
-import Control.Monad (liftM2)
 import Text.PrettyPrint
-import Data.Ord
+import Data.UUID
+import Data.UUID.Util
 
-import Data.Char (toUpper)
+newtype RegularIdentifier = RegularIdentifier UUID deriving (Eq,Ord)
 
--- https://msdn.microsoft.com/en-us/subscriptions/downloads/ms175874
-newtype RegularIdentifier = RegularIdentifier
-                            {
-                              unwrap :: String
-                            } 
-
-upCase :: String -> String
-upCase = map toUpper
-
-instance Eq RegularIdentifier where
-  a == b = (upCase $ unwrap a) == (upCase $ unwrap b)
-
-instance Ord RegularIdentifier where
-  compare = comparing (upCase . unwrap)
+unwrap :: RegularIdentifier -> String
+unwrap (RegularIdentifier x) = "UUID_" ++ xstr
+  where
+    xstr = filter (/= '-') (toString x)
 
 renderRegularIdentifier :: RegularIdentifier -> Doc
-renderRegularIdentifier (RegularIdentifier x) = text x
-
--- Removed hash; it's magic
-firstChars :: String
-firstChars = ['a'..'z'] ++ ['A'..'Z'] ++ "_"
-
-subsequentChars :: String
-subsequentChars = firstChars ++ ['0'..'9']
-
--- The MSDN documentation lies
--- The object or column name starting with '#IDZf39RtgmgnZgRrcVHc3d4nFsY9#UirNPVM6nICl8lubBsJfXjnV_yIPpagTXeYjN_2JrQwkUnPsBJ7OTqUtRn1E4xe65djU8WB#u7rM#MPdHVmi3QYk2gontf' is too long. The maximum length is 116 characters.
-maximumLengthOfRegularIdentifier :: Int
-maximumLengthOfRegularIdentifier = 116
-
-validLength :: String -> Bool
-validLength x = length x < maximumLengthOfRegularIdentifier
-
-validIdentifier :: String -> Bool
-validIdentifier x = validLength x && not (isReserved x)
-
--- https://msdn.microsoft.com/en-us/library/ms189822.aspx
-newtype Keyword = Keyword String
+renderRegularIdentifier = text . unwrap 
 
 instance Arbitrary RegularIdentifier where
   arbitrary = do
-    y <- liftM2
-         (:)
-         (elements firstChars)
-         (listOf (elements subsequentChars)) `suchThat` validIdentifier
-    return (RegularIdentifier y)
+    tl <- choose(minBound,maxBound)
+    tm <- choose(minBound,maxBound)
+    th <- choose(minBound,maxBound)
+    cshr <- choose(minBound,maxBound)
+    csl <- choose(minBound,maxBound)
+    n0 <- choose(minBound,maxBound)
+    n1 <- choose(minBound,maxBound)
+    n2 <- choose(minBound,maxBound)
+    n3 <- choose(minBound,maxBound)
+    n4 <- choose(minBound,maxBound)
+    n5 <- choose(minBound,maxBound)
+    let unpackedUUID = UnpackedUUID {
+          time_low = tl
+        , time_mid = tm
+        , time_hi_and_version = th
+        , clock_seq_hi_res = cshr
+        , clock_seq_low = csl
+        , node_0 = n0
+        , node_1 = n1
+        , node_2 = n2
+        , node_3 = n3
+        , node_4 = n4
+        , node_5 = n5
+        } 
+
+    return (RegularIdentifier (pack unpackedUUID))
