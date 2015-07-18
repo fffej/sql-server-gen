87e4430eb9edf4ac64e0042c191f81af0a5c3355
Jeff Foster <jeff.foster@acm.org>
2015-07-18 07:30:49 +0100

    basic scalar value functions working

--- src/Database/SqlServer/Types/Function.hs
+++ src/Database/SqlServer/Types/Function.hs
@@ -11,7 +11,7 @@ import Database.SqlServer.Types.Entity
 import Test.QuickCheck
 import Data.DeriveTH
 import Text.PrettyPrint
-import Data.Maybe (isJust)
+import Data.Maybe (isJust,fromJust)
 import Control.Monad
 
 data NullOption = ReturnsNullOnNullInput
@@ -28,11 +28,23 @@ data FunctionOption = FunctionOption
 
 derive makeArbitrary ''FunctionOption
 
+newtype InputType = InputType Type
+
+-- A time stamp can not be passed in as an argument to a function
+instance Arbitrary InputType where
+  arbitrary = liftM InputType $ arbitrary `suchThat` (not . isTimestamp)
+
+renderInputDataType :: InputType -> Doc
+renderInputDataType (InputType t) = renderDataType t
+
 data Parameter = Parameter
   {
     parameterName :: ParameterIdentifier
-  , dataType      :: Type
+  , dataType      :: InputType
   }
+
+renderParameter :: Parameter -> Doc
+renderParameter p = renderParameterIdentifier (parameterName p) <+> renderInputDataType (dataType p) 
 
 derive makeArbitrary ''Parameter
 
@@ -40,6 +52,13 @@ newtype ReturnType = ReturnType Type
 
 instance Arbitrary ReturnType where
   arbitrary = liftM ReturnType $ arbitrary `suchThat` (liftM isJust renderValue)
+
+renderReturnType :: ReturnType -> Doc
+renderReturnType (ReturnType t) = renderDataType t
+
+-- Safe because of instance of Arbitrary above
+renderReturnValue :: ReturnType -> Doc
+renderReturnValue (ReturnType t) = fromJust $ renderValue t
 
 data ScalarFunctionDefinition = ScalarFunctionDefinition
    {
@@ -55,3 +74,14 @@ derive makeArbitrary ''ScalarFunctionDefinition
 data FunctionDefinition = ScalarFunction ScalarFunctionDefinition
 
 derive makeArbitrary ''FunctionDefinition
+
+instance Entity FunctionDefinition where
+  toDoc (ScalarFunction f) = text "CREATE FUNCTION" <+> renderRegularIdentifier (scalarFunctionName f) <+>
+                             (parens $ hcat (punctuate comma (map renderParameter (parameters f)))) $+$
+                             text "RETURNS" <+> renderReturnType (returnType f) <+> text "AS" $+$
+                             text "BEGIN" $+$
+                             text "RETURN" <+> renderReturnValue (returnType f) $+$
+                             text "END" $+$ text "GO\n"
+
+instance Show FunctionDefinition where
+  show f = show (toDoc f)
