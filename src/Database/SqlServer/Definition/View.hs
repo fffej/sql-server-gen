ff0e6248c4e9d9513575c6392acde9438a765817
Jeff Foster <jeff.foster@acm.org>
2015-07-28 07:22:44 +0100

    view and metadata options

--- src/Database/SqlServer/Definition/View.hs
+++ src/Database/SqlServer/Definition/View.hs
@@ -18,6 +18,8 @@ data View = View
     viewName :: RegularIdentifier
   , tables :: [Table]
   , withCheckOption :: Bool
+  , encryption :: Bool
+  , viewMetadata :: Bool
   }
 
 unionSelectStatement :: View -> Doc
@@ -33,17 +35,30 @@ selectN t = text "SELECT" <+> hcat (punctuate comma cols) <+> text "FROM" <+> re
 renderPrerequisites :: View -> Doc
 renderPrerequisites = vcat . map toDoc . tables
 
+renderWithOptions :: View -> Doc
+renderWithOptions v
+  | noneSet = empty
+  | otherwise = text "WITH" <+>
+                hcat (punctuate comma (filter (not . (== empty)) [enc,vm]))
+  where
+    noneSet = not (encryption v) && not (viewMetadata v)
+    enc = if (encryption v) then text "ENCRYPTION" else empty
+    vm = if (viewMetadata v) then text "VIEW_METADATA" else empty
+
 instance Arbitrary View where
   arbitrary = do
     n <- arbitrary
     t <- arbitrary
     co <- arbitrary
-    return (View n [t] co)
+    e <- arbitrary
+    vm <- arbitrary
+    return (View n [t] co e vm)
 
 instance Entity View where
   name = viewName
   toDoc v = renderPrerequisites v $+$
             text "CREATE VIEW" <+> renderName v $+$
+            renderWithOptions v $+$
             text "AS" $+$
             unionSelectStatement v $+$
             (if (withCheckOption v) then text "WITH CHECK OPTION" else empty) $+$
