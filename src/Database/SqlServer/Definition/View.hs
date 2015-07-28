0172643c91d99e1b5a9a2ed149e59b8c08bd7c6b
Jeff Foster <jeff.foster@acm.org>
2015-07-28 07:12:05 +0100

    very simple version of views working

--- src/Database/SqlServer/Definition/View.hs
+++ src/Database/SqlServer/Definition/View.hs
@@ -8,19 +8,46 @@ module Database.SqlServer.Definition.View
 
 import Database.SqlServer.Definition.Identifier
 import Database.SqlServer.Definition.Entity
+import Database.SqlServer.Definition.Table
 
 import Test.QuickCheck
+import Text.PrettyPrint
 
 data View = View
   {
     viewName :: RegularIdentifier
+  , tables :: [Table]
+  , withCheckOption :: Bool
   }
+
+unionSelectStatement :: View -> Doc
+unionSelectStatement v = vcat $ punctuate (text " UNION ALL") (map selectN tabs)
+  where
+    tabs = tables v
+
+selectN :: Table -> Doc
+selectN t = text "SELECT" <+> hcat (punctuate comma cols) <+> text "FROM" <+> renderName t
+  where
+    cols = map (renderRegularIdentifier . columnName) $ unpack (columnDefinitions t)
+
+renderPrerequisites :: View -> Doc
+renderPrerequisites = vcat . map toDoc . tables
 
 instance Arbitrary View where
   arbitrary = do
     n <- arbitrary
-    return (View n)
+    t <- arbitrary
+    co <- arbitrary
+    return (View n [t] co)
 
 instance Entity View where
   name = viewName
-  toDoc = undefined
+  toDoc v = renderPrerequisites v $+$
+            text "CREATE VIEW" <+> renderName v $+$
+            text "AS" $+$
+            unionSelectStatement v $+$
+            (if (withCheckOption v) then text "WITH CHECK OPTION" else empty) $+$
+            text "GO\n"
+
+instance Show View where
+  show = show . toDoc
