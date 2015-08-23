857a27fa8fe4917f743c1f7ea47e863bff071afa
Jeff Foster <jeff.foster@acm.org>
2015-08-23 09:24:55 +0100

    errant )

--- src/Database/SqlServer/Create/Contract.hs
+++ src/Database/SqlServer/Create/Contract.hs
@@ -52,7 +52,7 @@ renderAuthorization n = text "AUTHORIZATION" <+> renderName n
 
 renderPrerequisites :: Contract -> Doc
 renderPrerequisites c = maybe empty render (authorization c) $+$
-                        map (render . messageType) (messageTypes c)) $+$
+                        map (render . messageType) (messageTypes c) $+$
                         text "GO\n"
 
 -- The service must have at least one message SENT BY INITIATOR or ANY.
