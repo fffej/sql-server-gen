e49d73bd30e92179852ba242ca5e82167cce78c5
Jeff Foster <jeff.foster@acm.org>
2015-08-23 09:25:39 +0100

    fix syntactical boo-boos

--- src/Database/SqlServer/Create/Database.hs
+++ src/Database/SqlServer/Create/Database.hs
@@ -80,7 +80,7 @@ renderDatabase dd = text "USE master" $+$
                     renderNamedEntities (credentials dd) $+$
                     renderNamedEntities (messages dd) $+$
                     renderNamedEntities (brokerPriorities dd) $+$
-                    renderNamedEntities (partitionFunctions dd) $+$
+                    renderNamedEntities (partitionFunctions dd)
   where
     dbName = renderName dd
 
