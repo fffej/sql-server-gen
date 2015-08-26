482a10d0e19271b255b57e36971c38adc2025ae4
Jeff Foster <jeff.foster@acm.org>
2015-08-26 11:53:37 +0100

    still need to generate the tvf for the security

--- src/Database/SqlServer/Create/SecurityPolicy.hs
+++ src/Database/SqlServer/Create/SecurityPolicy.hs
@@ -8,22 +8,55 @@ import Database.SqlServer.Create.Table
 import Database.SqlServer.Create.Entity
 
 import Test.QuickCheck
+import Text.PrettyPrint hiding (render)
 
+data SecuredObject = SecuredObject
+  {
+    table :: Table
+  , arguments :: [Column]
+  }
+
+instance Arbitrary SecuredObject where
+  arbitrary = do
+    t <- arbitrary
+    c <- sublistOf (unpack $ columns t) `suchThat` (not . null)
+    return (SecuredObject t c)
 
 data SecurityPolicy = SecurityPolicy
   {
     policyName :: RegularIdentifier
-  , tables :: [([Column], Table)]
+  , securedObjects :: [SecuredObject]
   , state :: Maybe Bool
   , forReplication :: Bool
   }
 
 instance Arbitrary SecurityPolicy where
-  arbitrary = undefined
+  arbitrary = SecurityPolicy <$>
+              arbitrary <*>
+              arbitrary <*>
+              arbitrary <*>
+              arbitrary
+
+renderFilterPredicates :: [SecuredObject] -> [Doc]
+renderFilterPredicates = map renderFilterPredicate
+
+renderFilterPredicate :: SecuredObject -> Doc
+renderFilterPredicate so =
+  text "ADD FILTER PREDICATE"
+
+renderState :: Bool -> Doc
+renderState True = text "WITH (STATE = ON)"
+renderState False = text "WITH (STATE = OFF)"
+          
 
 instance Entity SecurityPolicy where
   name = policyName
-  render = undefined
+  render s =
+    text "CREATE SECURITY POLICY" <+> (renderName s) $+$
+    vcat (renderFilterPredicates (securedObjects s)) $+$
+    maybe empty renderState (state s) $+$ 
+    if (not $ forReplication s) then text "NOT FOR REPLICATION" else empty $+$
+    text "GO\n"
 
 instance Show SecurityPolicy where
   show = show . render
